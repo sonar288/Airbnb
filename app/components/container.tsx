17db195d9111261655d8bfb34c1833e6c4935a91
Rahul Rajesh Sonar <96967994+sonar288@users.noreply.github.com>
2023-12-29 11:52:46 +0530

    made the ui for navbar

---
+++ app/components/container.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { Children } from "react";
+interface ContainerProps {
+    children: React.ReactNode;
+ } // thse are the props that we are going to pass to the component
+
+
+
+const Container : React.FC<ContainerProps> = ({children}) =>{
+    return(
+        <div className=" max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
+            {children}
+        </div>
+    );
+}
+export default Container;   
